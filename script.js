59ed06a69cf053ba8174617a976b096b396e7c09
Shpat Spahiu <137379379+shpatspahiu@users.noreply.github.com>
2023-08-10 21:23:37 +0200

    Create some useful vars
    Create some useful vars in order to calculate the num of pixels, their size, et..

--- script.js
+++ script.js
@@ -8,3 +8,8 @@ container.style = `width: ${windowWidth}px; height: ${windowWidth}px`;
 // CAUTION:
 // if another style is applied, the styles before this get 'overwritten'
 // container.style = 'background-color: blue;'
+
+// declaring some useful variables:
+let gridSize = 4; // n pixels per side
+let numOfPixels = gridSize ** 2; // total number of squares making up the grid
+let pixelWidth = windowWidth / gridSize; // width * width of pixels so that all of them fit the container

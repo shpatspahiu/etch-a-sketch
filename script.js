bbf518f6c4bcf59bd99d6fd78d2caf9a28bc2914
Shpat Spahiu <137379379+shpatspahiu@users.noreply.github.com>
2023-08-10 21:57:01 +0200

    Create pixels
    Create a method that creates n pixels and put them on display.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("Hello World");
-
 // Sketch window
 const windowWidth = 620;
 const container = document.querySelector(".container");
@@ -13,3 +11,29 @@ container.style = `width: ${windowWidth}px; height: ${windowWidth}px`;
 let gridSize = 4; // n pixels per side
 let numOfPixels = gridSize ** 2; // total number of squares making up the grid
 let pixelWidth = windowWidth / gridSize; // width * width of pixels so that all of them fit the container
+
+// createPixels creates n pixels and returns an array of them
+function createPixels(n, pixelWidth) {
+  const pixelElement = document.createElement("div");
+  pixelElement.setAttribute("class", "pixel");
+  //test
+  pixelElement.style = `width: 12px; height:12px;`;
+
+  // array holding the pixels
+  const pixelArray = [];
+
+  // fill array with necessary num of pixels
+  for (let i = 0; i < n; i++) {
+    pixelArray.push(pixelElement);
+  }
+
+  return pixelArray;
+}
+
+createPixels(1).forEach((pixel) => {
+  container.appendChild(pixel);
+});
+
+// const testParagraph = document.createElement("p");
+// testParagraph.textContent = 'Hello I"m a test paragraph';
+// container.appendChild(testParagraph);

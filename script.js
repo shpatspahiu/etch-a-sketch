3837180945710ae67f0a11088383f973cf23509c
Shpat Spahiu <137379379+shpatspahiu@users.noreply.github.com>
2023-08-10 21:09:11 +0200

    Set up window width and height

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 console.log("Hello World");
 
-
 // Sketch window
+const windowWidth = 620;
 const container = document.querySelector(".container");
+container.style = `width: ${windowWidth}px; height: ${windowWidth}px`;
+
+// CAUTION:
+// if another style is applied, the styles before this get 'overwritten'
+// container.style = 'background-color: blue;'
